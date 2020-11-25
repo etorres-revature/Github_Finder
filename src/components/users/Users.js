2f9702aa5d5f5c3efcf63a999060bc65dc2d3ac4
Eric D. Torres <etorresnotary@gmail.com>
2020-11-25 11:57:34 -0600

    adding spinner component to users loading

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,17 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import UserItem from "./UserItem";
+import Spinner from "../../components/Spinner/Spinner";
 
-class Users extends Component {
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.props.users.map((user) => {
+        {users.map((user) => {
           return <UserItem key={user.id} user={user} />;
         })}
       </div>
     );
   }
-}
+};
 
 const userStyle = {
   display: "grid",
