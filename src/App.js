17dc506ccfc07ee30dd739a8c8ac8f07da2cea2e
Eric D. Torres <etorresnotary@gmail.com>
2020-11-25 11:06:31 -0600

    components for user item and usr added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Component } from "react";
+import Navbar from "./components/layout/navbar/Navbar";
+import Users from "./components/users/Users";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar title="GitHub Finder!" icon="fab fa-github" />
+        <Users />
         <h1>Hello, from your React app</h1>
       </div>
     );
