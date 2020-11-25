a7cd1ae7fe237a0d36d08ae95fd9ea9014723c28
Eric D. Torres <etorresnotary@gmail.com>
2020-11-25 15:12:44 -0600

    repos added for full and final functionality

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,17 +1,21 @@
 import React, { Component, Fragment } from "react";
 import Spinner from "../Spinner/Spinner";
+import Repos from "../Repos/Repos";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
 
   static propTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
     getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
   };
 
   render() {
@@ -31,7 +35,7 @@ class User extends Component {
       hireable,
     } = this.props.user;
 
-    const { loading } = this.props;
+    const { repos, loading } = this.props;
 
     if (loading) return <Spinner />;
 
@@ -100,6 +104,7 @@ class User extends Component {
           </div>
           <div className="badge badge-danger">Public Gists: {public_gists}</div>
         </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
