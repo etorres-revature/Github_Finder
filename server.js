5898f08e35fac3809109eb130209ac90ab536506
Eric D. Torres <etorresnotary@gmail.com>
2020-11-25 22:01:41 -0600

    working on static middleware form whence it is pulled

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use("/public", express.static(__dirname + "../client/build"));
 
 app.listen(PORT, () => {
   console.log(`App is active at http://localhost:${PORT}`);
