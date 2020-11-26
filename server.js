49bab940757d4c0f15b998da7a128c7968e9089d
Eric D. Torres <etorresnotary@gmail.com>
2020-11-25 22:26:03 -0600

    more stack overflow to fix server issue

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/static", express.static(path.join(__dirname + "client/build")));
+app.use("/static", express.static(path.join(__dirname + "/client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`App is active at http://localhost:${PORT}`);
